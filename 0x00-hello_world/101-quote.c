b8cb133f696b1d9a56369318f45eda510a76874a
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-08 13:26:59 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *  * main - Contains my code
+ *   *
+ *    * Return: 1 if all goes well
+ *     */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+		return (1);
+
+}
