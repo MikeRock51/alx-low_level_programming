80cc2f5c13d5620cb1307c96cf6aab291ffccd79
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-08 14:01:29 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <unistd.h>>
 #include <stdio.h>
 /**
  * main - Contains my code
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-			return (1);
+		return (1);
 }
