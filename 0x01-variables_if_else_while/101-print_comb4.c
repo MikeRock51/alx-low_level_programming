90026eb374e484683745961d9ddf9c40285422be
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-09 15:17:06 -0700

    Comber

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - The entry point of the program
+ *
+ * Return: 0 if code works
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			for (k = 2; k < 10; k++)
+			{
+				if (k > j && j > i)
+				{
+					putchar(i % 10 + '0');
+					putchar(j % 10 + '0');
+					putchar(k % 10 + '0');
+					if (i != 7 || j != 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
