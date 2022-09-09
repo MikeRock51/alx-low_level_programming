62ece6c8cd45cae95480830be694f66e41a8f161
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-09 10:21:46 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - The entry point of the program
+ *
+ * Return: 0 if code works
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%i", n);
+	}
+
+	printf("\n");
+
+	return (0);
+}
