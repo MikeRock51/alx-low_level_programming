2f3eccfb163ebd79687b250ccc35d5c9ecea02b5
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-09 14:48:39 -0700

    comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point of the program
+ *
+ * Return: 0 if code works
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (j > i)
+			{
+				putchar(i % 10 + '0');
+				putchar(j % 10 + '0');
+
+				if (i <= 8 && j <= 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+
+}
