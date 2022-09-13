b3997c5d3cdbc18f1bfbf7a23b0d71b6b8b89987
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-13 16:30:17 -0700

    Adder

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Performs an addition of two ints
+ * @num1: first argument
+ * @num2: second argument
+ *
+ *Return: the sum of num1 and num2
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
