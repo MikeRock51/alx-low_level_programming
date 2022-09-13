107ebb08dd8565d5dea3a324a4530a521f396cd4
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-13 07:37:09 -0700

    Alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10x
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		int j;
+
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+
+	}
+	_putchar('\n');
+}
