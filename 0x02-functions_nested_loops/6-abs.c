2e3c848fe06cfac9f39b5fc181596eb304fc8c61
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-13 10:48:36 -0700

    Absolute Value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - calculates the absoluye value if an integer
+ * @j: takes an int j as input
+ *
+ * Returns: 0 always
+ */
+int _abs(int j)
+{
+	int i;
+	int v;
+
+	for (i = 0; i <= j; i++)
+	{
+		v += i;
+	}
+
+	return (v);
+}
