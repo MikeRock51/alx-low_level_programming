a00937dc44b722a18bdcc49b3025ffdd22e0e134
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-13 07:22:39 -0700

    Lower Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - The entry point of the program
+ *
+ * Return: 0 if code works
+ */
+void print_alphabet(void)
+{
+	for (int i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
