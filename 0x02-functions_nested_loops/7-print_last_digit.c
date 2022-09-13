eb0f2c8fe531dd7897331ac5438be74c9fb3899f
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-13 11:52:09 -0700

    Last Digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - calculates the absoluye value if an integer
+ * @n: takes an int n as input
+ *
+ * Return: 0 always
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
