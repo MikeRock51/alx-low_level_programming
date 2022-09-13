aa6a613841976cc2011bd56f169947dbb78ba200
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-13 06:59:43 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
-/*
- * main - The ebdtry point if my program
+/**
+ * main - Prints _putchar
  *
- * return: 0 if program works.
+ * Return: 0 if program works
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
