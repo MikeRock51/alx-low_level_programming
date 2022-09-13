76bad0ecdc0f26fa100bf8fbad84d1069210be1b
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-13 09:07:12 -0700

    Is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _isalpha - Checks if a character is an alphabet
+ * @c: Takes c as input
+ *
+ * Return: 1 if true.
+ * Return 0 otherwise
+ */
+int _isalpha(int c)
+{
+	int upper;
+	int lower;
+	int i;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		upper += i;
+	}
+
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		lower += j;
+	}
+
+	if (c == upper || c == lower)
+	{
+		return (0);
+	}
+
+	return (1);
+}
