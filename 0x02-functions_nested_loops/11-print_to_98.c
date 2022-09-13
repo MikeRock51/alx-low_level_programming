ba40e8ce6a21f67bcb5c7701b87f9843c4dd239b
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-13 16:57:19 -0700

    Print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Prints numbers and stops at 98, starting from n value
+ * @n: takes n as argument
+ */
+void print_to_98(int n)
+
+	while (n <= 98)
+	{
+		int print = n;
+
+		printf("%i, ", print);
+		print++;
+	}
+
+	_putchar('\n');
+}
