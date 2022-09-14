b5fd91b428e94e26c2accfab0e5eb6b646964b73
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-13 17:41:56 -0700

    Bug fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,27 +6,25 @@
  */
 void print_to_98(int n)
 {
-	int print = n;
+	int i, j;
 
-	if (n < 98)
+	if (n <= 98)
 	{
-		for (print = n; print <= 98; print++)
+		for (i = n; i <= 98; i++)
 		{
-			printf("%i, ", print);
-			n++;
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
 		}
 	}
-	else if (n > 98)
-	{
-		for (print = n; print >= 98; print--)
-		{	printf("%i, ", print);
-			n--;
-		}
-	}
-	else
-	{
-		printf("%i", print);
-	}
-
-	_putchar('\n');
 }
