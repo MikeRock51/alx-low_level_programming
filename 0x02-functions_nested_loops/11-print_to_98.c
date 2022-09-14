81327ea12965a7c0c4a1ee6a6aaf0514ac9840be
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-13 17:10:30 -0700

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,10 +10,16 @@ void print_to_98(int n)
 
 	while (n <= 98)
 	{
-		print = n;
-
 		printf("%i, ", print);
 		print++;
+		n++
+	}
+
+	while (n >= 98)
+	{
+		printf("%i, ", print);
+		print++;
+		n--;
 	}
 
 	_putchar('\n');
