6ef9ec2ec9e3c99d12ad9041966c6cc68ff01fbb
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-15 02:59:47 -0700

    Multiplier

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Calculates the maultiplication of two numbers
+ * @a: First input to be multiplied
+ * @b: Second input to be multiplied
+ *
+ * Return: 0 always
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
