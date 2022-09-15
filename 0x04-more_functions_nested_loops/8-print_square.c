f8c80aa1969d543f1d64863e48ff0fc5bf3bc3a2
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-15 12:06:20 -0700

    Bug fix on task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,20 +7,22 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
+	if (size > 0)
 	{
-		for (j = 0; j < size; j++)
+		for (i = 0; i < size; i++)
 		{
-			if (size > 0)
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
-			}
-			else
-			{
-				_putchar('\n');
-			}
-		}
 
+			}
+
+			_putchar('\n');
+			continue;
+		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
