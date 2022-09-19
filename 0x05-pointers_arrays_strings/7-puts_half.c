ce7a607d076507203e0486dda49288a48d2966b4
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-19 12:01:25 -0700

    Just half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts_half - Prints the second half of any given string
+ * @str: Taks as argument
+ */
+void puts_half(char *str)
+{
+	int length, half, i;
+
+	length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	half = length / 2;
+
+	i = 0;
+
+	for (i = half; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
