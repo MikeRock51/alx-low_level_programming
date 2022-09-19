543bf948e8bcdf43274f32b86e7973f26415398c
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-19 11:09:56 -0700

    String Reversal

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses any given string
+ * @s: Takes s as input
+ */
+void rev_string(char *s)
+{
+	int i, j, middle, length;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	length = i;
+	middle = i / 2;
+
+	char temp;
+
+	for (j = 0; j < middle; j++)
+	{
+		temp = s[j];
+		s[j] = s[length - j - 1];
+		s[length - j - 1] = temp;
+	}
+}
