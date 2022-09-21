40d122ec6d47a26ac04a9ae221cb9a507900ad5f
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-21 11:34:09 -0700

    ncopy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a given string
- * @s: Takes *s as input
- *
- * Return: The length of *S
- */
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; *s != '\0'; i++)
-	{
-		s++;
-	}
-
-	return (i);
-}
-
-/**
  * *_strncpy - Copies strings using n bytes
  * @dest: Copy string to
  * @src: Copy string for
@@ -28,18 +10,21 @@ int _strlen(char *s)
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int j;
 
-	i = 0;
 	j = 0;
 
 	while (j < n && src[j] != '\0')
 	{
-		dest[i] = src[j];
+		dest[j] = src[j];
 		j++;
-		i++;
 	}
 
-	dest[i] = '\0';
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
 	return (dest);
 }
