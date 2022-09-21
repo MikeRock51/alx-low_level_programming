54a98bfe9306fe9ee1136bccfb707caaf8ee8110
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-21 13:46:45 -0700

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,48 +1,23 @@
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a given string
- * @s: Takes *s as input
- *
- * Return: The length of *S
- */
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; *s != '\0'; i++)
-	{
-		s++;
-	}
-
-	return (i);
-}
-
-/**
  * _strcmp - Compares to strings
  * @s1: First string parameter
  * @s2: Secind string parameter
  *
  * Return: 0 if both strings are identical
  * Return positive value if s1 is > s2
- * Return negative value if s1 is less than s2 
+ * Return negative value if s1 is < s2
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
-
-	i = 0;
-
-	while (i < _strlen(s1) && i < _strlen(s2))
-	{
-		i++;
-	}
+	int j;
 
 	j = 0;
 
 	while (s1[j] == s2[j])
 	{
-		if (s1[j] == '\0' || s2[j] == '\0')
+		if (s1[j] == '\0')
 		{
 			return (0);
 		}
