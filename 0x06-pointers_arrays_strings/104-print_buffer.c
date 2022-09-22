15acae02dbacdf5044193c27767c84ba120e5f54
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-22 13:29:50 -0700

    Print Buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -7,40 +7,35 @@
  */
 void print_buffer(char *b, int size)
 {
-	int j, k, l;
-
-	if (size <= 0)
-	{
-		printf("\n");
-	}
-	else
-	{
-		for (j = 0; j < size; j += 10)
-		{
-			printf("%.8x:", j);
-
-			for (k = j; k < j + 10: k++)
-			{
-				if (k % 2 == 0)
-					printf(" ");
-				if (k < size)
-					printf("%.2x", *(b + k));
-				else
-				{
-					printf(" ");
-				}
-			printf(" ");
-			for (l = j; i < j + 10; i++)
-			{
-				if (l >= size)
-				break;
-				if(*(b + l) < 32 || *(b + l) > 126)
-					printf("%c", '.');
-				else
-					printf("%c", *(b +  l));
-			}
-				printf("\n");
-			}
-		}
-	}	
+int j, k, l;
+if (size <= 0)
+printf("\n");
+else
+{
+for (j = 0; j < size; j += 10)
+{
+printf("%.8x:", j);
+for (k = j; k < j + 10: k++)
+{
+if (k % 2 == 0)
+printf(" ");
+if (k < size)
+printf("%.2x", *(b + k));
+else
+{
+printf(" ");
+}
+printf(" ");
+for (l = j; i < j + 10; i++)
+{
+if (l >= size)
+break;
+if(*(b + l) < 32 || *(b + l) > 126)
+printf("%c", '.');
+else
+printf("%c", *(b +  l));
+}
+printf("\n");
+}
+}
 }
