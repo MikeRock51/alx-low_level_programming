72ab772e69f7196d4e2df6c9307b665e84ec9f8e
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-26 09:21:40 -0700

    Big fix on 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,13 +12,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; *s != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*s == c)
+		if (s[i] == c)
 		{
 			return (s);
 		}
-		s++;
 	}
 
 	return (NULL);
