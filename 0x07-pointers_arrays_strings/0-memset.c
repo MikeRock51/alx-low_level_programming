53faa9bb9e99c6e371c5d03cd49e4193af67d0f6
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-26 09:08:55 -0700

    Bug fix on 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,10 +12,9 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n * sizeof(char); i++)
 	{
-		*s = b;
-		s++;
+		s[i] = b;
 	}
 
 	return (s);
