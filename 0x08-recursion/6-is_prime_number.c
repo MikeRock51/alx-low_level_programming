68e42a40b6f9d97ee211b47b78a96707b4d45615
MikeRock51 <mikerockmusic51@gmail.com>
2022-09-28 12:00:55 -0700

    Prime Numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,54 @@
+iinclude "main.h"
+/**
+ * is_prime_number - Checks if a given number is prime or not
+ * @n: Given number
+ *
+ * Return: 1 if n is prime or 0 if n is now prime
+ */
+int is_prime_number(int n)
+{
+	int divider = 2;
+
+	if (n < 2)
+	{
+		return (0);
+	}
+	if (n % n == 0 || n % 1 == 0)
+	{
+		if (prime_number(divider, n) != 0)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
+
+/**
+ * prime_number - Helper function, recursive steps taken
+ * @n: number given to original function is_prime_number
+ * @divider: incrementer divisor
+ * Return: 0 if not prime, 1 if prime
+ */
+
+int prime_number(int divider, int n)
+{
+	if (divider < n)
+	{
+		if (n % divider == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			++divider;
+			return (prime_number(divider, n));
+		}
+	}
+	else
+	{
+		return (1);
+	}
+}
