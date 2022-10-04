4c2a5e6fd8f070e6ce413c029b2b5a0fb2f79cfa
MikeRock51 <mikerockmusic51@gmail.com>
2022-10-04 06:55:48 -0700

    Task 2 bug fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,29 +15,19 @@ char *str_concat(char *s1, char *s2)
 	int i, j, len1, len2;
 
 	if (s1 == NULL && s2 == NULL)
-	{
 		return (NULL);
-	}
 
 	if (s1 == NULL)
-	{
-		return (s2);
-	}
+		s1 = "";
 
 	if (s2 == NULL)
-	{
-		return (s1);
-	}
+		s2 = "";
 
 	for (i = 0, len1 = 0; s1[i]; i++)
-	{
 		len1++;
-	}
 
 	for (i = 0, len2 = 0; s2[i]; i++)
-	{
 		len2++;
-	}
 
 	cat = malloc(len1 + len2 * sizeof(char) + 1);
 
@@ -55,7 +45,6 @@ char *str_concat(char *s1, char *s2)
 	{
 		cat[i] = s2[j];
 	}
-
 	cat[i] = '\0';
 	return (cat);
 }
