f0e97788ef419367b31c7c35695c97fb430cf22a
MikeRock51 <mikerockmusic51@gmail.com>
2022-10-06 08:27:51 -0700

    Task 2 bug fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	arr = malloc(nmemb * sizeof(size));
+	arr = malloc(nmemb * size);
+	*arr = 0;
 
 	if (arr == NULL)
 		return (NULL);
