f39dc9ca510fb862c83c953fd0c02def0c6469dd
MikeRock51 <mikerockmusic51@gmail.com>
2022-10-06 10:16:14 -0700

    Task 5 debugged

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,10 +1,5 @@
 #include "main.h"
-
-int find_len(char *str);
-char *create_xarray(int size);
-char *iterate_zeroes(char *str);
-void get_prod(char *prod, char *mult, int digit, int zeroes);
-void add_nums(char *final_prod, char *next_prod, int next_len);
+#include <stdlib.h>
 
 /**
  * find_len - Finds the length of a string.
