a80697f68cd3a8caac0782457df70715fe82637b
MikeRock51 <mikerockmusic51@gmail.com>
2022-10-13 07:51:33 -0700

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,7 +4,7 @@
 
 /**
  * print_numbers - Prints all the numbers passed to it as argument
- * @seperator: String for seperating each number argument
+ * @separator: String for seperating each number argument
  * @n: Number of arguments passed
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%i", va_arg(args, unsigned int));
-		if (i < (n-1) && separator != NULL)
+		if (i < (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
