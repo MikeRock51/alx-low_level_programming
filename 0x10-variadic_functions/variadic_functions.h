72397b6d4b3839d84596860cc9d0d8b01e5a6886
MikeRock51 <mikerockmusic51@gmail.com>
2022-10-13 21:27:46 -0700

    Task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,5 +5,17 @@ int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * print_spec - A new struct that matches specifier to the appropriate function
+ * @speciier: The key to determine which print function to call
+ * @print: Pointing to the appropriate print function based on the specifier
+ */
+typedef struct printer
+{
+	char *specifier;
+	void (*print)(va_list args);
+} print_spec;
 
 #endif
