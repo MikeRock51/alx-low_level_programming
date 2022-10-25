201c1893ec5785ea7bd1820518cd701ca42f81ba
MikeRock51 <mikerockmusic51@gmail.com>
2022-10-25 05:27:56 -0700

    Task 5 done

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a linked list and set the head to NULL
+ * @head: A double pointer to the first node on the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	while (*head != NULL)
+	{
+		ptr = *head;
+		*head = (*head)->next;
+		free(ptr);
+	}
+	*head = NULL;
+}
