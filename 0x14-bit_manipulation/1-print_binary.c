dcf487c0b1ac2c54bd23bc9b48702a66c957c073
MikeRock51 <mikerockmusic51@gmail.com>
2022-10-27 10:41:41 -0700

    Task 1 bug fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,14 +5,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i, j;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	for (i = 31; i >= 0; i--)
-	{
-		j = n >> i;
-		if (j & 1)
-			_putchar('1');
-		else
-			_putchar('0');
-	}
+	_putchar((n & 1) + '0');
 }
