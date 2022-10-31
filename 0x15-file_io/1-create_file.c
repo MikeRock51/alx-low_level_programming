079c9c1ce4f9754ce62d9d7222d07d2c385c75d6
MikeRock51 <mikerockmusic51@gmail.com>
2022-10-31 14:46:42 -0700

    Task 1 Bug fixe

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,8 +14,11 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content[len])
-		len++;
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	write_len = write(fd, text_content, len);
