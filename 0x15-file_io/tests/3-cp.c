2d6af1e340b13b165e30195caa90b82c2b3c2ba4
MikeRock51 <mikerockmusic51@gmail.com>
2022-11-01 15:43:44 -0700

    Task 4

---
+++ 0x15-file_io/tests/3-cp.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/*
+ * main - Copies the content of a file to another file
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	int fd, o, r, w, c, d;
+	char *file_from = argv[1], *file_to = argv[2], *buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd = open(file_from, O_RDONLY);
+	r = read(fd, buffer, 1024);
+	o = open(file_to, O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (fd == -1 || r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		close(fd);
+		exit(98);
+	}
+	w = write(o, buffer, r);
+	if (o == -1 || w == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		close(fd);
+		exit(99);
+	}
+	c = close(fd);
+	d = close(o);
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", fd);
+		exit(100);
+	}
+	if (d == -1)
+	{
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", o);
+		exit(100);
+	}
+	return (0);
+}
---
+++ 0x15-file_io/tests/def.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%s\n", ARG_C_ERR);
+
+	return (0);
+}
