9262b79a9d2ccb74fbc8a47228da65da4ef6519f
MikeRock51 <mikerockmusic51@gmail.com>
2023-01-06 00:50:24 +0100

    Task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table
+ * @ht: Hash table to print
+ *
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0, first = 1;
+	hash_node_t *element;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	while (i < ht->size)
+	{
+		if (ht->array[i] != NULL)
+		{
+			element = ht->array[i];
+			if (!first)
+				printf(", ");
+			while (element != NULL)
+			{
+				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+				element = element->next;
+				if (element != NULL)
+					printf(", ");
+			}
+			first = 0;
+		}
+		i++;
+	}
+	printf("}\n");
+}
