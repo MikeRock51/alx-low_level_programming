b22a3105c0aa6972b933db45505bb8dd0dcea053
MikeRock51 <mikerockmusic51@gmail.com>
2023-01-05 16:08:26 +0100

    Task 0 init

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *new_hash = malloc(sizeof(hash_table_t));
+
+    if (new_hash == NULL)
+        return (NULL);
+
+    new_hash->size = size;
+    new_hash->array = malloc(sizeof(hash_node_t) * size);
+    
+    if (new_hash->array == NULL)
+        return (NULL);
+
+    return (new_hash);
+}
