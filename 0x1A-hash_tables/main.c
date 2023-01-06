8e52e62f110795cfc59437ae6dc75e51c15351fa
MikeRock51 <mikerockmusic51@gmail.com>
2023-01-06 09:01:49 +0100

    Fixing Task 3

--- 0x1A-hash_tables/main.c
+++ 0x1A-hash_tables/main.c
@@ -4,7 +4,7 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code for
+ * main - check the code
  *
  * Return: Always EXIT_SUCCESS.
  */
@@ -13,6 +13,14 @@ int main(void)
     hash_table_t *ht;
 
     ht = hash_table_create(1024);
-    printf("%p\n", (void *)ht);
+    hash_table_set(ht, "betty", "cool");
+    hash_table_set(ht, "hetairas", "mentioner");
+    hash_table_set(ht, "heliotropes", "neurospora");
+    hash_table_set(ht, "depravement", "serafins");
+
+    printf("%s\n", ht->array[key_index((unsigned char *)"betty", 1024)]->value);
+    printf("%s\n", ht->array[key_index((unsigned char *)"hetairas", 1024)]->value);
+    printf("%s\n", ht->array[key_index((unsigned char *)"heliotropes", 1024)]->value);
+    printf("%s\n", ht->array[key_index((unsigned char *)"depravement", 1024)]->value);
     return (EXIT_SUCCESS);
 }
