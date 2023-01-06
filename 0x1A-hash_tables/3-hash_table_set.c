415f0fe4862567ec757559b066fb2a201eb06308
MikeRock51 <mikerockmusic51@gmail.com>
2023-01-06 08:52:26 +0100

    Task 3 Fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_element = malloc(sizeof(hash_node_t));
 
-	if (new_element == NULL || strlen(key) < 1)
+	if (new_element == NULL || strlen(key) < 1 || ht == NULL || value == NULL)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
