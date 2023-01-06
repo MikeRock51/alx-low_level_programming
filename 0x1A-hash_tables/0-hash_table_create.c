74c2d24911012ffe3f16bc3a82dad81421b3337c
MikeRock51 <mikerockmusic51@gmail.com>
2023-01-06 08:32:29 +0100

    Task 1 Betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,12 @@
 #include "hash_tables.h"
-#include <stdlib.h>
 
+/**
+ * hash_table_create - Creates a hash table
+ * @size: The size of the array
+ *
+ * Return: A pointer to the created hash table
+ * Or NULL if something goes wrong
+*/
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash = malloc(sizeof(hash_table_t));
